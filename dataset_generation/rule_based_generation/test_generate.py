f37efce9609b9078537ee35ab96a0b4b676ea5d0
Tejvi M <tejvi@google.com>
2020-06-26 12:45:02 +0530

    split functions return list, writes are handled separately. updated tests

--- dataset_generation/rule_based_generation/test_generate.py
+++ dataset_generation/rule_based_generation/test_generate.py
@@ -5,30 +5,24 @@ import shutil
 
 
 class TestRawData(unittest.TestCase):
-    def test_write_permutations(self):
-        full_sentence = "a b c"
+    def test_get_permutations(self):
         chunks = ['a']
 
-        Generator("dummy_file",
-                  "output.txt").write_permutations(chunks, full_sentence)
+        result = Generator("dummy_file",
+                  "output.txt").get_permutations(chunks)
 
-        expected = [' a b c \t a <::::> \n', ' a b c \t <::::> a \n']
+        expected = ['a <::::>', '<::::> a']
 
-        result = open("output.txt", 'r').readlines()
-        os.remove("output.txt")
         self.assertEqual(expected, result)
 
     def test_write_pairs(self):
-        full_sentence = "a b c"
         chunks = ['a', 'b']
 
-        Generator("dummy_file",
-                  "output.txt").write_pairs(chunks, full_sentence)
+        result = Generator("dummy_file",
+                  "output.txt").get_pairs(chunks)
 
-        expected = ['a b c \t a <::::> b \n']
+        expected = [' a <::::> b ']
 
-        result = open("output.txt", 'r').readlines()
-        os.remove("output.txt")
         self.assertEqual(expected, result)
 
     def test_generate(self):
@@ -42,9 +36,9 @@ class TestRawData(unittest.TestCase):
 
         print("contents: ", contents)
         expected = [
-            'इसके अतिरिक्त गुग्गुल कुंड , भीम गुफा तथा भीमशिला भी दर्शनीय स्थल हैं । \t इसके अतिरिक्त स्थल <::::> गुग्गुल कुंड , भीम गुफा तथा भीमशिला भी स्थल \n',
-            'इसके अतिरिक्त गुग्गुल कुंड , भीम गुफा तथा भीमशिला भी दर्शनीय स्थल हैं । \t इसके अतिरिक्त स्थल <::::> दर्शनीय स्थल \n',
-            'इसके अतिरिक्त गुग्गुल कुंड , भीम गुफा तथा भीमशिला भी दर्शनीय स्थल हैं । \t गुग्गुल कुंड , भीम गुफा तथा भीमशिला भी स्थल <::::> दर्शनीय स्थल \n'
+            ' इसके अतिरिक्त गुग्गुल कुंड , भीम गुफा तथा भीमशिला भी दर्शनीय स्थल हैं । \t  इसके अतिरिक्त स्थल <::::> गुग्गुल कुंड , भीम गुफा तथा भीमशिला भी स्थल  \n',
+            ' इसके अतिरिक्त गुग्गुल कुंड , भीम गुफा तथा भीमशिला भी दर्शनीय स्थल हैं । \t  इसके अतिरिक्त स्थल <::::> दर्शनीय स्थल  \n',
+            ' इसके अतिरिक्त गुग्गुल कुंड , भीम गुफा तथा भीमशिला भी दर्शनीय स्थल हैं । \t  गुग्गुल कुंड , भीम गुफा तथा भीमशिला भी स्थल <::::> दर्शनीय स्थल  \n'
         ]
 
         os.remove("test_output.txt")
