31e30b45531c80dd6e1328de0e895395085174d4
Tejvi M <tejvi@google.com>
2020-07-03 15:26:25 +0530

    added function to drop random words from each of the splits, based on the specified percentage

--- dataset_generation/rule_based_generation/test_generate.py
+++ dataset_generation/rule_based_generation/test_generate.py
@@ -1,29 +1,58 @@
+import os
 import unittest
 from generate import Generator
-import os
-import shutil
 
 
 class TestRawData(unittest.TestCase):
     def test_get_permutations(self):
         chunks = ['a']
 
-        result = Generator("dummy_file",
-                  "output.txt").get_permutations(chunks)
+        result = Generator("dummy_file", "output.txt").get_permutations(chunks)
 
         expected = ['a <::::>', '<::::> a']
 
         self.assertEqual(expected, result)
 
-    def test_write_pairs(self):
+    def test_get_pairs(self):
         chunks = ['a', 'b']
 
-        result = Generator("dummy_file",
-                  "output.txt").get_pairs(chunks)
+        result = Generator("dummy_file", "output.txt").get_pairs(chunks)
 
         expected = [' a <::::> b ']
 
         self.assertEqual(expected, result)
+
+    def test_drop_words_pairs_all(self):
+        chunks = ['a', 'b']
+
+        result = Generator("dummy_file", "output.txt",
+                           drop_words_percentage=1).get_pairs(chunks)
+
+        print(result)
+        expected = ['  <::::>  ']
+
+        self.assertEqual(expected, result)
+
+    def test_drop_words_permutations_all(self):
+        chunks = ['a']
+
+        result = Generator("dummy_file", "output.txt",
+                           drop_words_percentage=1).get_permutations(chunks)
+
+        expected = ['  <::::>  ', '  <::::>  ']
+
+        self.assertEqual(expected, result)
+
+    def test_drop_words_pairs_half(self):
+        chunks = ['a', 'b']
+
+        result = Generator("dummy_file",
+                           "output.txt",
+                           drop_words_percentage=0.5).get_pairs(chunks)
+
+        first_sentence_words = len(result[0].split('<::::>')[0].split())
+
+        self.assertEqual(first_sentence_words, 1)
 
     def test_generate(self):
 
